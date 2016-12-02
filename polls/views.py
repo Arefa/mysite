80baf5baa54d28d8d1e0bd91f7b7f5b82a72c270
longlan <longlan.cqu@gmail.com>
2016-12-02 17:16:27 +0800

    3

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,7 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
 from .models import Question
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
@@ -14,10 +13,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
