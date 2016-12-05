64eb74189ab4ed9a2975e7bf3129038825c3d761
longlan <longlan.cqu@gmail.com>
2016-12-05 17:32:30 +0800

    6

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
@@ -14,6 +15,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
